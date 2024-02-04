b1fadcdaa9f33cda3805d9d1bead27f900797ce5
amjadkhan345 <62803260+amjadkhan345@users.noreply.github.com>
2024-02-04 20:08:12 +0300

    Update app_chat.py

--- app_chat.py
+++ app_chat.py
@@ -3,8 +3,8 @@ import os
 import joblib
 import streamlit as st
 import google.generativeai as genai
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 GOOGLE_API_KEY="AIzaSyB90zUQDGx82iJBKVU66s439Me_qxJanJs" #os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
